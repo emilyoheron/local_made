f0e632e3b12c976878daccd050a7f8d1e59c7ec7
Emily O'Heron <89399844+emilyoheron@users.noreply.github.com>
2023-11-30 19:09:42 -0600

    changes made to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,53 @@
 'use client'
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Database } from '@/app/lib/database.types'
+import { useEffect, useState } from 'react'
+import { Session } from '@supabase/auth-helpers-nextjs'
 
 const PublicPage = () => {
   const router = useRouter();
+  const supabase = createClientComponentClient<Database>();
+  const [session, setSession] = useState<Session|null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
 
   const handleArtistLogin = () => {
-    // Redirect to the artist login page
     router.push('/login');
+  };
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    setSession(null);
   };
 
   return (
     <div>
       <header>
         <h1>Artists Showcase</h1>
-        <button onClick={handleArtistLogin}>Artist Login</button>
+        <div>
+          {session ? (
+            <>
+              <button onClick={handleLogout}>Logout</button>
+              <button onClick={() => router.push('/account')}>Account</button>
+            </>
+          ) : (
+            <button onClick={handleArtistLogin}>Artist Login</button>
+          )}
+        </div>
       </header>
       {/* Display artists and their profiles here */}
       <main>
@@ -25,3 +58,5 @@ const PublicPage = () => {
 };
 
 export default PublicPage;
+
+
